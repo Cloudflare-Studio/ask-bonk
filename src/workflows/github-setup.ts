19f4b9f34ed400c28b8cbed0ecd84bce931a38b7
Matt Silverlock <matt@eatsleeprepeat.net>
2026-06-29 12:55:00 -0400

    Update dependencies (#195)
    update dependencies

--- src/workflows/github-setup.ts
+++ src/workflows/github-setup.ts
@@ -1,10 +1,25 @@
-import type { FlueContext } from "@flue/runtime";
+import { defineWorkflow } from "@flue/runtime";
+import * as v from "valibot";
 import { runSetupWorkflowJob } from "../github-workflow-jobs";
-import { internalWorkflowRoute } from "../internal-workflows";
-import type { Env, SetupWorkflowRequest } from "../types";
+import {
+  internalWorkflowAgent,
+  internalWorkflowRoute,
+  runInternalWorkflowJob,
+  workflowJobResultSchema,
+} from "../internal-workflows";
 
 export const route = internalWorkflowRoute;
 
-export async function run({ env, payload }: FlueContext<SetupWorkflowRequest, Env>) {
-  return await runSetupWorkflowJob(env, payload);
-}
+export default defineWorkflow({
+  agent: internalWorkflowAgent,
+  input: v.object({
+    owner: v.string(),
+    repo: v.string(),
+    issue_number: v.number(),
+    default_branch: v.string(),
+  }),
+  output: workflowJobResultSchema,
+  async run(context) {
+    return await runInternalWorkflowJob(context, (env) => runSetupWorkflowJob(env, context.input));
+  },
+});
